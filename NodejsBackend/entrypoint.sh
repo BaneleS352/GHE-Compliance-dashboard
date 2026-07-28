35b84486d69ab2b352d818069cdf4810c4c9c7b0
NDAEDZO AUSTIN MUKHUBA <NMukhuba@hollywoodbets.net>
2026-07-28 11:13:03 +0200

    docker3

--- NodejsBackend/entrypoint.sh
+++ NodejsBackend/entrypoint.sh
@@ -6,5 +6,8 @@ mkdir -p /app/uploads
 echo "Running Prisma db push..."
 npx prisma db push --skip-generate
 
+echo "Seeding database..."
+node /app/dist/seed.js
+
 echo "Starting server..."
 node dist/index.js
